cb2d120219c604429febc01a4d241e0a6776e56f
Antonina Nesterenko <injamed@gmail.com>
2015-09-17 23:07:48 -0700

    data shaped to work with near operator

--- 2_Data_Wrangling_with_mongoDB/Project3_Data_wrangling_MongoDB/data_wranglling_1.py
+++ 2_Data_Wrangling_with_mongoDB/Project3_Data_wrangling_MongoDB/data_wranglling_1.py
@@ -1,10 +1,13 @@
+__author__ = 'injamed'
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import xml.etree.cElementTree as ET
-import pprint
 import re
 import codecs
 import json
+
+
 """
 Your task is to wrangle the data and transform the shape of the data
 into the model we mentioned earlier. The output should be a list of dictionaries
@@ -37,12 +40,12 @@ You have to complete the function 'shape_element'.
 We have provided a function that will parse the map file, and call the function with the element
 as an argument. You should return a dictionary, containing the shaped data for that element.
 We have also provided a way to save the data in a file, so that you could use
-mongoimport later on to import the shaped data into MongoDB. 
+mongoimport later on to import the shaped data into MongoDB.
 
 Note that in this exercise we do not use the 'update street name' procedures
 you worked on in the previous exercise. If you are using this code in your final
-project, you are strongly encouraged to use the code from previous exercise to 
-update the street names before you save them to JSON. 
+project, you are strongly encouraged to use the code from previous exercise to
+update the street names before you save them to JSON.
 
 In particular the following things should be done:
 - you should process only 2 types of top level tags: "node" and "way"
@@ -50,7 +53,7 @@ In particular the following things should be done:
     - attributes in the CREATED array should be added under a key "created"
     - attributes for latitude and longitude should be added to a "pos" array,
       for use in geospacial indexing. Make sure the values inside "pos" array are floats
-      and not strings. 
+      and not strings.
 - if second level tag "k" value contains problematic characters, it should be ignored
 - if second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
 - if second level tag "k" value does not start with "addr:", but contains ":", you can process it
@@ -102,16 +105,16 @@ def shape_element(element):
         node["type"] = element.tag
 
         if "lat" in element.attrib or "lon" in element.attrib:
-            node["pos"] = [0, 0]
+            node["loc"] = {"type" : "Point", "coordinates": [0, 0]}
 
         for attrName, attrValue in element.attrib.items():
             if attrName in CREATED:
                 node["created"][attrName] = attrValue
 
             elif attrName == "lat":
-                node["pos"][0] = float(attrValue)
+                node["loc"]["coordinates"][1] = float(attrValue)
             elif attrName == "lon":
-                node["pos"][1] = float(attrValue)
+                node["loc"]["coordinates"][0] = float(attrValue)
             else:
                 node[attrName] = attrValue
 
@@ -146,7 +149,7 @@ def shape_element(element):
 
 def process_map(file_in, pretty = False):
     # You do not need to change this file
-    file_out = "{0}.json".format(file_in)
+    file_out = "{0}_2ds.json".format(file_in)
     data = []
     with codecs.open(file_out, "w", "utf-8") as fo:
         for _, element in ET.iterparse(file_in):
@@ -166,37 +169,5 @@ def read_xml_attr(node, attr):
     else:
         return u""
 
-def my_test():
-    # NOTE: if you are running this code on your computer, with a larger dataset, 
-    # call the process_map procedure with pretty=False. The pretty=True option adds 
-    # additional spaces to the output, making it significantly larger.
-    data = process_map('kyiv_ukraine.osm', False)
-    # with open("kyiv_ukraine.json", "w") as f:
-    #     #f.write(data)
-    #     for item in data:
-    #         print>>f, item
-    #pprint.pprint(data)
-    
-    # correct_first_elem = {
-    #     "id": "261114295",
-    #     "visible": "true",
-    #     "type": "node",
-    #     "pos": [41.9730791, -87.6866303],
-    #     "created": {
-    #         "changeset": "11129782",
-    #         "user": "bbmiller",
-    #         "version": "7",
-    #         "uid": "451048",
-    #         "timestamp": "2012-03-28T18:31:23Z"
-    #     }
-    # }
-    # assert data[0] == correct_first_elem
-    # assert data[-1]["address"] == {
-    #                                 "street": "West Lexington St.",
-    #                                 "housenumber": "1412"
-    #                                   }
-    # assert data[-1]["node_refs"] == [ "2199822281", "2199822390",  "2199822392", "2199822369",
-    #                                 "2199822370", "2199822284", "2199822281"]
-
 if __name__ == "__main__":
-    my_test()
+    process_map('kyiv_ukraine_sample.osm', False)
